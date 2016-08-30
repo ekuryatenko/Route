10cd222999757a5d4c381525f9880199c8150fbd
shpp-ekuryatenko <ekuryatenko@gmail.com>
2016-08-30 17:32:00 +0300

    has to be deployed to heroku

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 'use strict';
 const Hapi = require('hapi');
 
+var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+
 const server = new Hapi.Server();
-server.connection({ port: 3000 });
+server.connection({port: process.env.PORT});
 
 server.route(
     [
@@ -31,18 +33,40 @@ server.start((err) => {
 });
 
 function pathHandler(reply, name){
-     reply ("HELLO!!" + name);
+    reply ("HELLO!!  " + name);
 }
 
 /** Error handler support */
 function emailHandler(request, reply){
+    reply("OK!");
+
     var data = request.payload;
 
-    console.log("FROM: " + data.envelope.from);
-    console.log("TO: " + data.envelope.to);
-    console.log("SUBJECT: " + data.headers.Subject);
-    console.log("TEXT: " + data.plain);
-    console.log("****************************************");
+    var incomingFrom = data.envelope.from;
+    var incomingTo = data.envelope.to;
+    var incomingSubject =  data.headers.Subject;
+    var incomingPlain =  data.plain;
 
-    reply("OK!");
+    console.log("GET MESSAGE FROM:" + incomingFrom);
+
+    /** SENDGRID PART */
+    var helper = sg.mail;
+    var from_email = new helper.Email('HEROKU_APP');
+    var to_email = new helper.Email(incomingFrom);
+    var subject = 'EMAIL_APP RESPONSE';
+    var content = new helper.Content('text/plain', 'Hello, Email!');
+    var mail = new helper.Mail(from_email, subject, to_email, content);
+
+    var sgRequest = sg.emptyRequest({
+        method: 'POST',
+        path: '/v3/mail/send',
+        body: mail.toJSON()
+    });
+
+    sg.API(sgRequest, function(error, response) {
+        console.log(response.statusCode);
+        console.log(response.body);
+        console.log(response.headers);
+    });
+
 }
