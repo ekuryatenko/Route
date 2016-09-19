ea3411112c724fe10de9b8ff604a38cc343f9be7
ekuryatenko <ekuryatenko@gmail.com>
2016-09-19 23:02:21 +0300

    client/login comments

--- client/logIn.js
+++ client/logIn.js
@@ -1,60 +1,60 @@
-/** ======================
+/**************************
  * Get page fields
- * =======================*/
+ **************************/
 
-// selector function
-var getNode = function(s) {
-  return document.querySelector(s);
+// Returns reference to selector page object
+var getNode = function(selector) {
+  return document.querySelector(selector);
 };
 
-// get the form nodes
-var USER_EMAIL_INPUT = getNode('#user_email_input'),
-  FPASS_INPUT = getNode('#password_first'),
-  LOGIN_BUTTON = getNode('#logInButton'),
-  SIGNIN_BUTTON = getNode('#signInButton');
+// Get the form nodes
+var USER_EMAIL_INPUT = getNode("#user_email_input"),
+  FPASS_INPUT = getNode("#password_first"),
+  LOGIN_BUTTON = getNode("#logInButton"),
+  SIGNIN_BUTTON = getNode("#signInButton");
 
-/** ======================
+/**************************
  * On page load events
- * =======================*/
+ **************************/
 
 // Connection to start html page server source
 var SERVER_URL = window.location.hostname;
-var socket = io.connect (SERVER_URL);
+var socket = io.connect(SERVER_URL);
 
-/** ======================
+/**************************
  * From server side events
- * =======================*/
+ **************************/
 
-// alert error messages returned from the server
-socket.on('alert', function(msg){
+// Fires to show server message
+socket.on("alert", function(msg) {
   alert(msg);
 });
 
-// clear the login form
-socket.on('clean-form', function(){
-  USER_EMAIL_INPUT.value = '';
-  FPASS_INPUT.value = '';
+// Fires to clear the login form
+socket.on("clean-form", function() {
+  USER_EMAIL_INPUT.value = "";
+  FPASS_INPUT.value = "";
 });
 
-// Switch user to destination page
-socket.on('redirect', function(destination) {
-  USER_EMAIL_INPUT = "";
-  FPASS_INPUT = "";
+// Switches user to new destination page
+socket.on("redirect", function(destination) {
+  USER_EMAIL_INPUT.value = "";
+  FPASS_INPUT.value = "";
 
   //It was impossible to save whole profile object in sessionStorage
   var signIn_obj = socket.profile.user_email;
-  sessionStorage.setItem('ss_user_profile', signIn_obj);
+  sessionStorage.setItem("ss_user_profile", signIn_obj);
 
   window.location.href = destination;
 });
 
-/** ======================
+/**************************
  * Page side events
- * =======================*/
+ **************************/
 
-// add the event listener for the login submit button
-LOGIN_BUTTON.addEventListener('click', function(event){
-  // create variables to send to the server and assign them values
+// Initiates login data sending to server on submit button
+LOGIN_BUTTON.addEventListener('click', function(event) {
+  // Variables to send to the server
   var user_email = USER_EMAIL_INPUT.value,
     fpass = FPASS_INPUT.value;
 
@@ -63,18 +63,20 @@ LOGIN_BUTTON.addEventListener('click', function(event){
     fpass: fpass
   };
 
+  // Store session data while server in processing
   socket.profile = logIn_obj;
 
-  // send the values to the server
-  socket.emit('logIn', logIn_obj);
+  // Send the values to the server
+  socket.emit("logIn", logIn_obj);
 
+  // Prevents page reloading
   event.preventDefault();
 });
 
-// add the event listener for the login submit button
-SIGNIN_BUTTON.addEventListener('click', function(){
-  USER_EMAIL_INPUT.value = '';
-  FPASS_INPUT.value = '';
+// Initiates signin page loading on button click
+SIGNIN_BUTTON.addEventListener("click", function() {
+  USER_EMAIL_INPUT.value = "";
+  FPASS_INPUT.value = "";
 
   window.location.href = "/signInForm.html";
 });
