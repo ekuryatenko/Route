092205d83097a4ef97f4b13e1547f5eb1fa73ee9
shpp-ekuryatenko <ekuryatenko@gmail.com>
2017-02-14 15:03:25 +0200

    nodemailer refactroing

--- src/helpers/nodemailerHelper.js
+++ src/helpers/nodemailerHelper.js
@@ -5,71 +5,8 @@ var smtpTransport = require('nodemailer-smtp-transport');
 const PATTERN = /{{userName}}/g;
 
 module.exports = function () {
-  let self = this;
   return {
-    sendEmail: sendEmail,
-    sendEmail2: sendEmail2,
-
-    /**
-     * Sends emails to all receivers of param arr in cycle
-     * Prepares email text for every user
-     * Carries sending statistics to resume result of
-     * sending in param callback
-     *
-     * Haven't managed with promises in cycle here
-     *
-     * @param {Array} arr - Users profiles
-     * @param {string} mainText - Main app reply text
-     * @param {Function} callback - On whole sending cycle finish
-     */
-    sendEmailsInCycle: (sendOptions) => {
-      return new Promise((resolve, reject) => {
-        let emailsCnt = 0;
-        let successfulEmail = 0;
-
-        let users = sendOptions.users;
-        let text = sendOptions.text;
-
-        for (var item of users) {
-          text = text.replace(PATTERN, item.emailTemplate);
-
-          // setup e-mail data with unicode symbols
-          var mailOptions = {
-            from    : process.env.NODE_MAILER_SENDER_EMAIL, // sender address
-            to      : item.user_email,                        // list of receivers
-            subject : "ROUTE EMAIL",                     // Subject line
-            text    : text,                                 // plaintext body
-            html    : ""                                    // html body
-          };
-
-          emailsCnt++;
-
-          try {
-            console.log("SENDING!!!!!!!!!");
-            this.sendEmail2(mailOptions).then(
-              (success)=> {
-                successfulEmail++;
-
-                if (emailsCnt == users.length) {
-                  let alert = "IT WERE SENDED " + successfulEmail + " EMAILS OF " + emailsCnt;
-                  console.log(alert);
-                  resolve(alert);
-                }
-              },
-              (fail) => {
-                console.error(fail);
-                let alert = "IT WERE SENDED " + successfulEmail + " EMAILS OF " + emailsCnt;
-                reject(e + ": " + alert);
-              }
-            );
-          }catch(e){
-            let alert = "IT WERE SENDED " + successfulEmail + " EMAILS OF " + emailsCnt;
-            reject(e + ": " + alert);
-          }
-        }
-      });
-    },
-
+    sendEmail2        : sendEmail2,
     sendEmailsInCycle2:sendEmailsInCycle2
   }
 }();
@@ -111,7 +48,7 @@ function sendEmail (emailOptions) {
   });
 }
 
-function sendEmail2 (mailOptions, transporter) {
+function sendEmail2 (mailContent, transporter) {
   return new Promise((resolve, reject) => {
     /**
      * Invalid login error - use captcha: https://github.com/nodemailer/nodemailer#using-gmail
@@ -119,32 +56,85 @@ function sendEmail2 (mailOptions, transporter) {
     console.log("---------EMAIL SENT");
 
     // send mail with defined transport object
+    transporter.sendMail(mailContent, (err, info) => {
+      if (err) {
+        reject(err);
+      }
 
-      transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-          // TODO: Arrange correct error logging to console
-          reject(err);
-        }
-        try{
-          resolve('Message sent: ' + info.response);
-        }catch(e){
-          reject(e);
-        }
+      try{
+        resolve('Message sent: ' + info.response);
+      }catch(unHandledErr){
+        reject(unHandledErr);
+      }
     })
   });
 }
 
+function sendEmailsInCycle (sendOptions) => {
+  return new Promise((resolve, reject) => {
+    let emailsCnt = 0;
+    let successfulEmail = 0;
 
-function* sendEmailsInCycle2(sendOptions) {
+    let users = sendOptions.users;
+    let text = sendOptions.text;
 
+    for (var item of users) {
+      text = text.replace(PATTERN, item.emailTemplate);
+
+      // setup e-mail data with unicode symbols
+      var mailOptions = {
+        from    : process.env.NODE_MAILER_SENDER_EMAIL, // sender address
+        to      : item.user_email,                        // list of receivers
+        subject : "ROUTE EMAIL",                     // Subject line
+        text    : text,                                 // plaintext body
+        html    : ""                                    // html body
+      };
+
+      emailsCnt++;
+
+      try {
+        console.log("SENDING!!!!!!!!!");
+        this.sendEmail2(mailOptions).then(
+          (success)=> {
+            successfulEmail++;
+
+            if (emailsCnt == users.length) {
+              let alert = "IT WERE SENDED " + successfulEmail + " EMAILS OF " + emailsCnt;
+              console.log(alert);
+              resolve(alert);
+            }
+          },
+          (fail) => {
+            console.error(fail);
+            let alert = "IT WERE SENDED " + successfulEmail + " EMAILS OF " + emailsCnt;
+            reject(e + ": " + alert);
+          }
+        );
+      }catch(e){
+        let alert = "IT WERE SENDED " + successfulEmail + " EMAILS OF " + emailsCnt;
+        reject(e + ": " + alert);
+      }
+    }
+  });
+}
+
+/**
+ * Sends emails to all receivers of param arr in cycle
+ * Prepares unique email text for every user
+ * Carries sending statistics to resume result of
+ * sending in param callback
+ *
+ * @param {Object} dbData - db Content: users and text
+ */
+function* sendEmailsInCycle2(dbData) {
   let successfulEmail = 0;
 
-  let users = sendOptions.users;
-  let text = sendOptions.text;
+  let users     = dbData.users;
+  let text      = dbData.text;
   let emailsQty = users.length;
 
   // create reusable transporter object using the default SMTP transport
-  var transporter = nodemailer.createTransport(smtpTransport({
+  let transporter = nodemailer.createTransport(smtpTransport({
     service: 'gmail',
     auth: {
       user: process.env.NODE_MAILER_SENDER_EMAIL,
@@ -154,11 +144,11 @@ function* sendEmailsInCycle2(sendOptions) {
 
   console.log("SENDING!!!!!!!!!");
 
-  for (var item of users) {
+  for (let item of users) {
     text = text.replace(PATTERN, item.emailTemplate);
 
-    // setup e-mail data with unicode symbols
-    var mailContent = {
+    // setup email content
+    let mailContent = {
       from    : process.env.NODE_MAILER_SENDER_EMAIL, // sender address
       to      : item.user_email,                      // list of receivers
       subject : "ROUTE EMAIL",                        // Subject line
@@ -176,24 +166,4 @@ function* sendEmailsInCycle2(sendOptions) {
   }
 
   console.log("IT WERE SENDED " + successfulEmail + " EMAILS OF " + emailsQty);
-}
-
-function sendEmail3 (mailOptions, transporter) {
-    /**
-     * Invalid login error - use captcha: https://github.com/nodemailer/nodemailer#using-gmail
-     */
-    console.log("---------EMAIL SENT");
-
-    // send mail with defined transport object
-    try{
-     return transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          // TODO: Arrange correct error logging to console
-          console.error('SENDING ERROR: ' + error);
-        }
-       console.log('Message sent: ' + info.response);
-      })}
-    catch(e){
-      console.error('BIG SENDING ERROR: ' + e);
-    }
 }
